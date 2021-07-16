9125db2b3c4c4b816fd9f6a0deff06005254501b
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-07-15 22:36:26 -0500

    Primer commit

---
+++ PersonaPOO/src/Persona.java
@@ -0,0 +1,15 @@
+
+public class Persona {
+	String name;
+	int age;
+	
+	public String saludar() {
+		return "Hola" + this.name;
+	}
+	
+	public String miEdad() {
+		return "Mi edad es" + this.age;
+	}
+	
+
+}
---
+++ PersonaPOO/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona carlos = new Persona();
+		carlos.name = "Carlos Eduardo";
+		carlos.age = 30;
+		System.out.println(carlos.saludar());
+		System.out.println(carlos.miEdad());
+		
+		
+	}
+
+}
